c4755239275c0717146448d033631f7e5d83b128
ahmad-frehan <ahmad-frehan@futurecode-co.com>
2023-04-11 13:03:13 +0300

    api home users

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:test/home/getx/home_controller.dart';
@@ -14,8 +13,15 @@ class HomeScreen extends GetView<HomeController> {
           controller.getUsers();
         },
         child: Container(
-
-
+          child: ListView.builder(
+              itemCount: controller.users.value.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(controller.users.value[index].name.toString()),
+                  leading: CircleAvatar(
+                      child: Text(controller.users.value[index].id.toString())),
+                );
+              }),
         ),
       ),
     );
